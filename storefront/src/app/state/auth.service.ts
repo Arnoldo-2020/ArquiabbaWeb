01201fbb4558097144b6b644b4b8ffd28d23ae51
Arnoldo José <70756194+Arnoldo-2020@users.noreply.github.com>
2025-10-04 00:07:38 +0200

    setting authService and enviroments for production

--- storefront/src/app/state/auth.service.ts
+++ storefront/src/app/state/auth.service.ts
@@ -1,17 +1,32 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private http = inject(HttpClient);
+  private base = environment.API_URL;
 
   login(email: string, password: string) {
-    return this.http.post('/api/auth/login', { email, password });
+    return this.http.post(
+      `${this.base}/auth/login`,
+      { email, password },
+      { withCredentials: true }
+    );
   }
+
   logout() {
-    return this.http.post('/api/auth/logout', {});
+    return this.http.post(
+      `${this.base}/auth/logout`,
+      {},
+      { withCredentials: true }
+    );
   }
+
   me() {
-    return this.http.get<{ id:string; email:string; role:'ADMIN'|'USER' }>('/api/auth/me');
+    return this.http.get(
+      `${this.base}/auth/me`,
+      { withCredentials: true }
+    );
   }
 }
--- storefront/src/environments/environment.ts
+++ storefront/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true, // en prod
+  API_URL: 'https://arquibabbaweb.onrender.com/api'
 };
 
 /*
