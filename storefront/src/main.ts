d76afdfe8f83390aa7687199b73eb2785f24c513
Arnoldo José <70756194+Arnoldo-2020@users.noreply.github.com>
2025-10-07 00:26:11 +0200

    main.ts modification for prod

--- storefront/src/main.ts
+++ storefront/src/main.ts
@@ -1,3 +1,4 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -5,9 +6,14 @@ import { provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 import { authCsrfInterceptor } from './app/shared/auth-csrf.interceptor';
 import { errorToastInterceptor } from './app/shared/error-toast.interceptor';
 
+// Esta sección activa optimizaciones para el entorno de producción
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
