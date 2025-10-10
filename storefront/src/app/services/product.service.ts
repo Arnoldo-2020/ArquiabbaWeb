3c8cfedeaf720336ddbbe09a5558f961e4995339
Arnoldo José <70756194+Arnoldo-2020@users.noreply.github.com>
2025-10-11 00:32:54 +0200

    private base fix

--- storefront/src/app/services/product.service.ts
+++ storefront/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { environment } from 'src/environments/environment';
 
 type CreateOrUpdatePayload = {
   name: string;
@@ -14,7 +15,7 @@ type CreateOrUpdatePayload = {
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
-  private base = '/api/products';
+  private base = `${environment.API_URL}/products`;
 
   // ------- GETS -------
   list(): Observable<Product[]> {
@@ -26,17 +27,14 @@ export class ProductsService {
   }
 
   // ------- CREATE -------
-  create(data: CreateOrUpdatePayload): Observable<Object> {
-    // const body = new FormData();
-    // body.set('name', data.name);
-    // body.set('description', data.description);
-    // body.set('price', String(data.price));
-    // if (data.image)    body.set('image', data.image);
-    // if (data.imageUrl) body.set('imageUrl', data.imageUrl);
-    // return this.http.post<Product>(this.base, body);
-    console.log('Enviando a la ruta de PRUEBA /api/products-test');
-    const testPayload = { name: 'Prueba', price: 10 };
-    return this.http.post(`${this.base}/products-test`, testPayload);
+  create(data: CreateOrUpdatePayload): Observable<Product> {
+    const body = new FormData();
+    body.set('name', data.name);
+    body.set('description', data.description);
+    body.set('price', String(data.price));
+    if (data.image)    body.set('image', data.image);
+    if (data.imageUrl) body.set('imageUrl', data.imageUrl);
+    return this.http.post<Product>(this.base, body);
   }
 
   // ------- UPDATE -------
